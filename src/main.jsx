bc28f256887914c0efe97ed3edb6c17cb2e8a277
siwat jermwatthana <siwat.j@hiveground.com>
2024-02-01 20:12:48 +0700

    [Feat]: Add UI Register Form

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import App2 from "./App2.jsx";
+import RegisterForm from "./components/Form/Register.jsx";
+
 import "./tailwind.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* <App /> */}
-    <App2 />
+    {/* <App2 /> */}
+    <RegisterForm />
   </React.StrictMode>
 );
