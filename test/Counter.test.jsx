babf257e317da4ace71aaba5b4790fa3a4200e64
siwat jermwatthana <siwat.j@hiveground.com>
2024-02-04 15:37:02 +0700

    [Feat]: Add unit test counter

---
+++ test/Counter.test.jsx
@@ -0,0 +1,23 @@
+import { describe, it, expect, beforeEach } from "vitest";
+import { render, screen, fireEvent } from "@testing-library/react";
+import Counter from "../src/components/Counter/Counter";
+
+describe("Counter Component", () => {
+  beforeEach(() => {
+    render(<Counter />);
+  });
+
+  it("should render counter", () => {
+    expect(screen.getByText(/Counters:/)).toBeInTheDocument();
+  });
+
+  it("increments counter", () => {
+    fireEvent.click(screen.getByText("Increment"));
+    expect(screen.getByText("Counter: 1")).toBeInTheDocument();
+  });
+
+  it("decrements counter", () => {
+    fireEvent.click(screen.getByText("Decrement"));
+    expect(screen.getByText("Counter: -1")).toBeInTheDocument();
+  });
+});
